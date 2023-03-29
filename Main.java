e6fef41397ee80e3ecbd81086a1d9fbc33aa3945
169721 <126687475+169721@users.noreply.github.com>
2023-03-29 13:28:33 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+  String znak = "*";
+  for(int i=1; i<=5; i++)
+  {
+    for(int a=1; a<=5; a++)
+    System.out.print(znak);
+     System.out.println(" ");
+  }
+for(int i=1; i<=5; i++)
+  {
+    int a=1;
+    if()
+    System.out.print(znak);
+     System.out.println(" ");
+  }
+
+}
+}
+
+ 
